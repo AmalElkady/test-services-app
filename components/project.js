a03a1a00019a7f67db6465f8a60452097005480e
AmalElkady <AmalElkady@github.com>
2020-09-25 17:20:04 +0200

    edit project-data

--- components/project.js
+++ components/project.js
@@ -2,6 +2,9 @@ export default function Project({ project }) {
   return (
     <li>
       <p>{project.name}</p>
+      {/* <div className="img-dev">
+        <img src={project.image}></img>
+      </div> */}
     </li>
   );
 }
